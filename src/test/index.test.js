6d11b067bfd8c5bb908162b26df77254a11be8d5
James Cape <james.cape@gmail.com>
2019-09-05 09:19:33 -0600

    Adjusts directory path so index.html can be found

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,9 +2,9 @@ import { expect } from "chai"
 import sinon from "sinon"
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("Top word and its count", () => {
+    it("outputs the top word and how many times it occurs", () => {
+      
     })
   })
 })
-
